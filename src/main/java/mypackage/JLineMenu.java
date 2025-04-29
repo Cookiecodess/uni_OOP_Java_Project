01ea7a6b178235bc66363ffafbbf1e8d5658b6c0
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-04-29 18:06:52 +0800

    Add product and category pages

--- src/main/java/mypackage/JLineMenu.java
+++ src/main/java/mypackage/JLineMenu.java
@@ -12,6 +12,7 @@ import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 
 import java.util.Scanner;
+import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -123,14 +124,14 @@ public class JLineMenu {
     private boolean hasExitOption;
     
     int firstItemIdx = 0;
-    int selected = 0; // want to access in subclass
+    int currentSelection = 0; // want to access in subclass
     private boolean running = true;
     
     // Public constructor of a JLineMenu object
-    public JLineMenu(String textHeader, ArrayList<String> options, String textPrompt, boolean hasBackOption, boolean hasExitOption) {
+    public JLineMenu(String textHeader, List<String> options, String textPrompt, boolean hasBackOption, boolean hasExitOption) {
 
         this.textHeader = textHeader;
-        this.options = (ArrayList<String>) options.clone(); // Make a copy of the options ArrayList, so we don't modify the original ArrayList 
+        this.options = new ArrayList<>(options); 
         this.textPrompt = textPrompt;
         this.hasBackOption = hasBackOption;       
         this.hasExitOption = hasExitOption;
@@ -148,8 +149,8 @@ public class JLineMenu {
     
     public void drawOptions() {
         for (int i = firstItemIdx; i < numOfOptions; i++) {
-            if (i == selected) {
-                // Highlight selected item
+            if (i == currentSelection) {
+                // Highlight currentSelection item
                 terminal.writer().println("\u001b[7m> " + this.options.get(i) + "\u001b[0m");
             } else {
                 terminal.writer().println("  " + this.options.get(i));
@@ -158,13 +159,13 @@ public class JLineMenu {
     }
     
     public void moveCursorUp() {
-        selected = (selected - 1 + this.numOfOptions) % this.numOfOptions;
+        currentSelection = (currentSelection - 1 + this.numOfOptions) % this.numOfOptions;
     }
     public void moveCursorDown() {
-        selected = (selected + 1) % this.numOfOptions;
+        currentSelection = (currentSelection + 1) % this.numOfOptions;
     }
     public void onLeft() {}; // default do nothing
-    public void onRight() {}; // default do nothing
+    public void onRight() {}; // default do nothing// default do nothing
     
     /**
      * Draws a menu that's navigable with arrow keys!
@@ -223,31 +224,31 @@ public class JLineMenu {
                     case 13 -> { // Enter key pressed
                         clearScreen();
                         
-                        // "Back" or "Exit" selected?
+                        // "Back" or "Exit" currentSelection?
                         if (this.hasBackOption && this.hasExitOption) {
                             // Exit
-                            if (selected == this.numOfOptions - 1) {
+                            if (currentSelection == this.numOfOptions - 1) {
                                 confirmExit();
                                 continue; // If user returns here, it means they chose to cancel exiting the program.
                             }
                             // Back
-                            else if (selected == this.numOfOptions - 2) {
+                            else if (currentSelection == this.numOfOptions - 2) {
                                 System.out.println(SHOW_CUR); // Show cursor
                                 return BACK_OPTION;
                             }
                         } 
-                        else if (this.hasBackOption && selected == this.numOfOptions - 1) { // Back
+                        else if (this.hasBackOption && currentSelection == this.numOfOptions - 1) { // Back
                             System.out.println(SHOW_CUR); // Show cursor
                             return BACK_OPTION;
                         }
-                        else if (this.hasExitOption && selected == this.numOfOptions - 1) { // Exit
+                        else if (this.hasExitOption && currentSelection == this.numOfOptions - 1) { // Exit
                             confirmExit();
                             continue; // If user returns here, it means they chose to cancel exiting the program.
                         }
                         
-                        // An option other than "Back" or "Exit" is selected, so return the index of selected option.
+                        // An option other than "Back" or "Exit" is currentSelection, so return the index of currentSelection option.
                         System.out.println(SHOW_CUR); // Show cursor
-                        return selected;
+                        return currentSelection;
                     }
                     default -> {
                         // Clear input buffer
@@ -326,14 +327,29 @@ public class JLineMenu {
     public void setOptions(ArrayList<String> options) {
         this.options = options;
     }
+    
+    public int getCurrentSelection() {
+        return this.currentSelection;
+    }
+    public void setCurrentSelection(int currSel) {
+        this.currentSelection = currSel;
+    }
+    
+    public int getNumOfOptions() {
+        return this.numOfOptions;
+    }
+    public void setNumOfOptions(int numOfOptions) {
+        this.numOfOptions = numOfOptions;
+    }
+    
     public static void sound(){
     
-    try{
-             Runtime.getRuntime().exec("powershell -c [console]::beep(1000,500)");//HZ& SECOND
-             
-             }catch(IOException e){
-             e.printStackTrace();
-             }
-    
-    }
+        try{
+                 Runtime.getRuntime().exec("powershell -c [console]::beep(1000,500)");//HZ& SECOND
+                 
+                 }catch(IOException e){
+                 e.printStackTrace();
+                 }
+        
+        }
 }
--- src/main/java/mypackage/PaginatedMenu.java
+++ src/main/java/mypackage/PaginatedMenu.java
@@ -11,31 +11,57 @@ import java.util.ArrayList;
  * @author User
  */
 public class PaginatedMenu extends JLineMenu {
+    private int totalNumOfItems;
     private int totalPages;
     private int itemsPerPage;
     private int page = 0; // current page
     
     public PaginatedMenu(String textHeader, ArrayList<String> options, String textPrompt, boolean hasBack, boolean hasExit, int itemsPerPage) {
         super(textHeader, options, textPrompt, hasBack, hasExit);
-        super.numOfOptions = this.itemsPerPage = itemsPerPage; // note: superclass constructor assigns total size of options to numOfOptions 
-        this.totalPages = options.size() / this.itemsPerPage; // integer division -> floors the quotient
+        this.itemsPerPage = itemsPerPage; 
+        super.setNumOfOptions(this.itemsPerPage); // note: superclass constructor assigns total size of options to numOfOptions 
+        this.totalNumOfItems = options.size();
+        this.totalPages = this.totalNumOfItems / this.itemsPerPage; // integer division -> floors the quotient
     }
     
     @Override
     public void onLeft() {
-        this.page = (this.page - 1 + this.totalPages) % this.totalPages;
+        prevPage();
         updatePage();
     }
     @Override
     public void onRight() {
-        this.page = (this.page+1) % this.totalPages;
+        nextPage();
         updatePage();
+    }
+    @Override
+    public void moveCursorUp() {
+        if (super.getCurrentSelection() < this.getFirstItemIdx()) {
+            if (this.page == 0) {
+                this.page = this.totalPages - 1; // jump to last page
+                super.setCurrentSelection(this.totalNumOfItems - 1); // jump to last item
+            } else {
+                
+            }
+        }
+    }
+    
+    public void prevPage() {
+        this.page = (this.page - 1 + this.totalPages) % this.totalPages;
+    }
+    public void nextPage() {
+        this.page = (this.page+1) % this.totalPages;
     }
     
     
     public void updatePage() {
         super.firstItemIdx = getFirstItemIdx();
-        super.selected = 0;
+        super.setCurrentSelection(0);
+        if (this.page == this.totalPages - 1) {
+            // if it's the last page
+            int lastPageItemCount = this.totalNumOfItems % this.itemsPerPage;
+            super.setNumOfOptions(lastPageItemCount);
+        }
     }
     
     public int getFirstItemIdx() {
